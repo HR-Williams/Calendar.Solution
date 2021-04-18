9eed8ee09f4c3df647fa9e39293b22fe8c56c91e
HR Williams <halrubinwilliams@gmail.com>
2021-04-18 14:00:10 -0700

    Set up test file

---
+++ Calendar.Tests/ModelTests/LeapYearTests.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calendar;
+
+namespace Calendar.TestTools
+{
+  [TestClass]
+  public class LeapYearTests
+  {
+    [TestMethod]
+    // Test method will go here
+  }
+}
